763ad47ff5b517f1d3338997f18d1204f39d7311
bayazidsarour <sarourkhan3@gmail.com>
2025-02-24 19:51:21 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -5,17 +5,34 @@
 //     link.href = `${location.protocol}//${location.host}/css/dark.css`
 // })
 
+// let dark = document.querySelector(".dark");
+// let link = document.querySelector('.css');
+
+// dark.addEventListener("click", () => {
+    
+//     dark.classList.toggle("active");
+
+   
+//     if (dark.classList.contains("active")) {
+//         link.href = `${location.protocol}//${location.host}/css/dark.css`;
+//     } else {
+//         link.href = `${location.protocol}//${location.host}/css/style.css`;
+//     }
+// });
+
+
 let dark = document.querySelector(".dark");
 let link = document.querySelector('.css');
 
 dark.addEventListener("click", () => {
-    
+    // Toggle the 'active' class on the button
     dark.classList.toggle("active");
 
-   
+    // If the 'active' class exists, enable dark mode; otherwise, light mode
     if (dark.classList.contains("active")) {
-        link.href = `${location.protocol}//${location.host}/css/dark.css`;
+        // Use a relative path for GitHub Pages
+        link.href = "./css/dark.css";
     } else {
-        link.href = `${location.protocol}//${location.host}/css/style.css`;
+        link.href = "./css/style.css";
     }
 });
